90fa7563a5807d1c148645e6fc516e1ebf750e04
Kristupas Rybakovas <kristupas.rybakovas@gmail.com>
2018-08-12 21:17:47 +0300

    Init commit

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html'
+})
+export class MessagesComponent implements OnInit {
+
+  constructor(public messageService: MessageService) {
+    
+  }
+
+  ngOnInit() {
+    console.log(this.messageService);
+  }
+
+}
