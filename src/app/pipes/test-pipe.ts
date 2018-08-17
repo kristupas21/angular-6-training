3b6bdc217a7af7593e059cc1a8b33215b4100a5e
Kristupas Rybakovas <kristupas.rybakovas@devbridge.com>
2018-08-17 10:41:12 +0300

    Fix reversed pipe

--- src/app/pipes/test-pipe.ts
+++ src/app/pipes/test-pipe.ts
@@ -3,6 +3,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'reversed' })
 export class ReversedPipe implements PipeTransform {
   transform(value: string): string {
+    if (!value) {
+      return '';
+    }
+
+    if (typeof value !== 'string') {
+      return value;
+    }
+
     const valueArr = value.split('');
     const reversedArr = valueArr.reverse();
 
