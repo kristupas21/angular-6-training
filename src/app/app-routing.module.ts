3eeb8495b12478fa0562dca32723488847892098
Kristupas Rybakovas <kristupas.rybakovas@gmail.com>
2018-09-30 16:16:53 +0300

    Delete components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsComponent } from './components/display-container/projects/projects.component';
-import { CaseBuilderComponent } from './components/display-container/case-builder/case-builder.component';
-import { LabelExpComponent } from './components/display-container/label-exp/label-exp.component';
-import { FilterSetupComponent } from './components/display-container/filter-setup/filter-setup.component';
-import { LoginComponent } from './components/login/login.component';
-import { MainComponent } from './components/main/main.component';
-import { CanActivateTeam, Permissions } from '@app-services/can-activate.service';
-import { UserToken } from '@app-services/user-token.service';
 
 export const childRoutes: Routes = [
-  { path: '', redirectTo: '/main/(menu:projects)', pathMatch: 'full'},
-  { path: 'projects',           component: ProjectsComponent,     outlet: 'menu' },
-  { path: 'case-builder',       component: CaseBuilderComponent,  outlet: 'menu' },
-  { path: 'label-expectation',  component: LabelExpComponent,     outlet: 'menu' },
-  { path: 'filter-setup',       component: FilterSetupComponent,  outlet: 'menu' }
+  
 ];
 
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/main', pathMatch: 'full'},
-  { path: 'main',   component: MainComponent, canActivate: [CanActivateTeam], children: childRoutes },
-  { path: 'login',  component: LoginComponent },
+  
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(appRoutes), RouterModule.forChild(childRoutes) ],
+  imports: [  ],
   exports: [ RouterModule ],
   providers: [
-    CanActivateTeam, Permissions, UserToken
+    
   ]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,22 +12,11 @@ import { MatIconModule } from '@angular/material/icon';
 
 /* Internal */
 // Modules
-import { AppRoutingModule } from './app-routing.module';
 // Components
 import { AppComponent } from './app.component';
-import { DashboardContainerComponent } from './components/dashboard-container/dashboard-container.component';
-import { NavContainerComponent } from './components/nav-container/nav-container.component';
-import { DisplayContainerComponent } from './components/display-container/display-container.component';
 import { IconNotificationComponent } from './common/icons/icon-notification/icon-notification.component';
 import { IconUserComponent } from './common/icons/icon-user/icon-user.component';
 import { IconChevronRightComponent } from './common/icons/icon-chevron-right/icon-chevron-right.component';
-import { NavigationComponent } from './components/nav-container/navigation/navigation.component';
-import { NavLinkComponent } from './components/nav-container/navigation/nav-link/nav-link.component';
-import { CaseBuilderComponent } from './components/display-container/case-builder/case-builder.component';
-import { ProjectsComponent } from './components/display-container/projects/projects.component';
-import { LabelExpComponent } from './components/display-container/label-exp/label-exp.component';
-import { FilterSetupComponent } from './components/display-container/filter-setup/filter-setup.component';
-import { ProjectListComponent } from './components/display-container/projects/project-list/project-list.component';
 // Reducers
 import { sideNavReducer, selectedNameReducer } from './reducers';
 
@@ -39,19 +28,9 @@ import { UsersStore } from './akita/user.store';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardContainerComponent,
-    NavContainerComponent,
-    DisplayContainerComponent,
     IconNotificationComponent,
     IconUserComponent,
-    NavigationComponent,
-    NavLinkComponent,
     IconChevronRightComponent,
-    CaseBuilderComponent,
-    ProjectsComponent,
-    LabelExpComponent,
-    FilterSetupComponent,
-    ProjectListComponent,
     UserComponent
 
   ],
@@ -59,7 +38,6 @@ import { UsersStore } from './akita/user.store';
     CommonModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatSelectModule,
